125d3c9ac284d500994f4df742c3f26451421070
f29 javi <javier.logrono@outlook.com>
2023-01-03 10:59:51 +0100

    QUESTIONS ON ENGLISH

--- services/translation.js
+++ services/translation.js
@@ -40,6 +40,25 @@ function getTranslationDictionary (req, res){
   });
 }
 
+function getTranslationDictionaryInvert (req, res){
+  var lang = req.body.lang;
+  var category = config.translationCategory;
+  var info = req.body.info;
+  var translationKey = config.translationKey;
+  request.post({url:'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&&from=en&to='+lang+'&category='+category,json: true,headers: {'Ocp-Apim-Subscription-Key': translationKey, 'Ocp-Apim-Subscription-Region': 'northeurope' },body:info}, (error, response, body) => {
+    if (error) {
+      console.error(error)
+      res.status(500).send(error)
+    }
+    if(body=='Missing authentication token.'){
+      res.status(401).send(body)
+    }else{
+      res.status(200).send(body)
+    }
+
+  });
+}
+
 function getTranslationSegments(req, res){
     var lang = req.body.lang;
     var category = config.translationCategory;
@@ -62,5 +81,6 @@ function getTranslationSegments(req, res){
 module.exports = {
   getDetectLanguage,
   getTranslationDictionary,
+  getTranslationDictionaryInvert,
   getTranslationSegments
 }
