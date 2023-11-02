b24e8b4387ccf97a3295cd6acd680f61e3d988eb
Javier <javierlogronoaguirre@gmail.com>
2023-11-02 15:00:23 +0100

    mandatory origin

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,11 @@ const api = express.Router()
 
 // Lista de dominios permitidos
 const whitelist = ['https://dxgpt.app'];
+//const whitelist = ['https://dxgpt.app', 'http://localhost:4200'];
 const corsOptions = {
     origin: function (origin, callback) {
         console.log(origin)
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
+      if (whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
