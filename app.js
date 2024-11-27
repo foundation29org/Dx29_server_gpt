20ff6747c948f2cdffa33948e72081df2b2eaf0e
Javier <javierlogronoaguirre@gmail.com>
2024-11-27 12:27:15 +0100

    TEST csp

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
         defaultSrc: ["'self'"],
+        scriptSrcAttr: ["'unsafe-inline'"],
         scriptSrc: [
             "'self'",
             "'unsafe-inline'",
